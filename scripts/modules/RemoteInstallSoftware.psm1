01985f9a81deb3a0ca387adff554e74a9468fc4d
Daniel Rapp <daniel.rapp@live.se>
2019-01-25 11:20:23 +0100

    added windows update module

--- scripts/modules/RemoteInstallSoftware.psm1
+++ scripts/modules/RemoteInstallSoftware.psm1
@@ -18,23 +18,25 @@ Function Download() {
         [Parameter(Position=6,Mandatory=$True)]
         [PSCredential]$Cred
     )
-
-    if ($UseProxy){
-        try{
-            Invoke-RestMethod -Method Get -Uri $Url -OutFile $Outpath -UseDefaultCredentials -Proxy $ENV:Proxyserver
-         }
-         catch{
-            throw "Download failed with proxy $Proxyserver" 
-         }
-    }
-    else {
-        try{
-            Invoke-RestMethod -Method Get -Uri $Url -ea stop -OutFile $Outpath
-         }
-         catch{
-            throw "Download failed" 
-         }
-    }
+    Invoke-Command -ComputerName $ComputerName -Credential $Cred -ScriptBlock {
+        if ($UseProxy){
+            try{
+                Invoke-RestMethod -Method Get -Uri $Url -OutFile $Outpath -UseDefaultCredentials -Proxy $Proxyserver
+             }
+             catch{
+                throw "Download failed with proxy $Proxyserver" 
+             }
+        }
+        else {
+            try{
+                Invoke-RestMethod -Method Get -Uri $Url -ea stop -OutFile $Outpath
+             }
+             catch{
+                throw "Download failed" 
+             }
+        } 
+    } -ArgumentList
+    
 }
 
 
@@ -44,18 +46,12 @@ Function InstallMSI() {
         [string]$ComputerName,
 
         [Parameter(Position=2,Mandatory=$True)]
-        [string]$URL,
-
-        [Parameter(Position=3,Mandatory=$True)]
         [string]$Path,
 
-        [Parameter(Position=4,Mandatory=$False)]
+        [Parameter(Position=3,Mandatory=$False)]
         [string]$ArgumentList,
 
-        [Parameter(Position=5,Mandatory=$True)]
-        [string]$Proxyserver,
-
-        [Parameter(Position=6,Mandatory=$True)]
+        [Parameter(Position=4,Mandatory=$True)]
         [PSCredential]$Cred
     )
     $p = Start-Process -Wait `
@@ -68,4 +64,29 @@ Function InstallMSI() {
         throw "Installing $Path failed."
     }
 
+}
+
+Function Invoke-DownloadAndInstall() {
+    Param(
+        [Parameter(Position=1,Mandatory=$True)]
+        [string]$ComputerName,
+
+        [Parameter(Position=2,Mandatory=$True)]
+        [string]$URL,
+
+        [Parameter(Position=3,Mandatory=$True)]
+        [string]$Outpath,
+
+        [Parameter(Position=4,Mandatory=$False)]
+        [switch]$UseProxy,
+
+        [Parameter(Position=5,Mandatory=$True)]
+        [string]$Proxyserver,
+        [Parameter(Position=6,Mandatory=$True)]
+        [string]$ArgumentList,
+        [Parameter(Position=7,Mandatory=$True)]
+        [PSCredential]$Cred
+    )
+    Invoke-Command -ComputerName $ComputerName -Credential $Cred -ScriptBlock ${Function:Download} -ArgumentList $ComputerName,$URL,$Outpath,$UseProxy,$Proxyserver,$Cred
+    Invoke-Command -ComputerName $ComputerName -Credential $Cred -ScriptBlock ${Function:InstallMSI} -ArgumentList $ComputerName,$Outpath,$ArgumentList,$Cred
 }
